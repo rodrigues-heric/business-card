690a2f14bc24850718529019a7a104db2e02923b
Heric Leite Rodrigues <hericleiterodrigues@gmail.com>
2026-01-21 17:00:59 -0300

    Feat: adding business card (#1)

--- business-card/src/App.tsx
+++ business-card/src/App.tsx
@@ -1,5 +1,5 @@
-function App() {
-  return <></>;
-}
+import { BusinessCard } from "./components/BusinessCard";
 
-export default App;
+export default function App() {
+  return <BusinessCard />;
+}
